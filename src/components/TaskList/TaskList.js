a227634b3160a605ad5a3b0f1b3f3f6027d04f51
lmedrek <lukasz.medrek12@gmail.com>
2023-12-07 22:23:32 +0100

    feature: implement advanced tasks (without ability to remove tasks from list) and fix: move components from TitleText to index.js

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,12 +1,25 @@
+/* eslint-disable react/prop-types */
 import './TaskList.module.scss';
 
 import React from 'react';
 
 export function DisplayTaskList({tasks}){
     
-    
-    
-    return{
-
+    const drawX= (pressed) =>{
+        const buttonID = pressed.target.id;
+        if(document.getElementById(buttonID).innerHTML){document.getElementById(buttonID).innerHTML="";document.getElementById(buttonID.slice(0,-3)).style.textDecoration="none";} 
+        else{document.getElementById(buttonID).innerHTML='✓';document.getElementById(buttonID.slice(0,-3)).style.textDecoration="line-through";}
     }
+
+    function DisplayTaskList(){
+        const content=[];
+        tasks.forEach(element => {
+            content.push(<><div style={{display: "flex", alignItems: "center", justifyContent:"center"}}><li id={element.id}>{element.txt}</li> <button id={element.id+"box"} onClick={drawX}></button></div></>);
+        });
+        return content;
+    }
+    
+    return(
+        <><div><ol>{DisplayTaskList()}</ol></div><hr></hr></>
+    );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
-import { TitleText } from '../components/TitleText/TitleText';
-
+import { DisplayTask} from '../components/CheckContent/CheckContent.js';
+import { DisplayCounter} from '../components/Counter/Counter.js';
+import { DisplayDynamicList} from '../components/DynamicList/DynamicList.js';
+import { SimpleForm } from '../components/SimpleForm/SimpleForm.js';
+import { DisplayTaskList } from '../components/TaskList/TaskList.js';
+import { TitleText } from '../components/TitleText/TitleText.js';
+const listOfTasks = [{id:1, txt:"Task 1"},{id:2, txt:"Task 2"},{id:3, txt:"Task 3"},{id:4, txt:"Task 4"},{id:5, txt:"Task 5"}];
 const IndexPage = () => (
   <>
     <TitleText />
+    <DisplayTask />
+    <DisplayCounter/>
+    <DisplayDynamicList n={4}/>
+    <DisplayTaskList tasks={listOfTasks}/>
+    <SimpleForm/>
   </>
 );
 
